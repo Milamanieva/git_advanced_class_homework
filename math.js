7349ab0760ced0126a7bae809b7af4d5ac3fd228
Milamanieva <milamanieva@gmail.com>
2018-09-26 15:15:32 +0200

    Updated

--- math.js
+++ math.js
@@ -7,8 +7,13 @@ const subtractNumbers = function(a,b) {
   return a-b;
 };
 
+const divideNumbers = function(a,b){
+	return a/b;
+};
+
 
 console.log(addNumbers());
 
 console.log(subtractNumbers());
 
+console.log(divideNumbers());
